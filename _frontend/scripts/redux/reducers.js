17a114359903020809fec89924caa5ed86868b55
Daniel Ahn <danielahn@DAs-MacBook-Pro.attlocal.net>
2018-04-25 01:43:05 -0700

    admins can now add customers; add custObj to redux;  redirects to customer details with props data from redux; updated sign up with activity create; changes in redux as needed for viewing customer details

--- _frontend/scripts/redux/reducers.js
+++ _frontend/scripts/redux/reducers.js
@@ -1,17 +1,30 @@
 const initialState = {
+    loggedInAdmin: '',
+    customerObject: ''
 }
 
 const rootReducer = (state = initialState, action) => {
-    // switch (action.type) {
-    //     case ("SET_LOGIN_SESSION"):
-    //         state = {
-    //             ...state,
-    //             loggedInUser: action.payload
-    //         }
-    //         break;
-    // }
-    
-    // return state;
+    switch (action.type) {
+        case ("SET_LOGIN_SESSION"):
+            state = {
+                ...state,
+                loggedInUser: action.payload
+            }
+            break;
+        case ("CLEAR_LOGIN_SESSION"):
+            state = {
+                loggedInUser: action.payload
+            }
+            break;
+        case ("SET_CUSTOMER_OBJECT"):
+            state = {
+                ...state,
+                customerObject: action.payload
+            }
+            break;
+    }
+
+    return state;
 }
 
 export default rootReducer;
