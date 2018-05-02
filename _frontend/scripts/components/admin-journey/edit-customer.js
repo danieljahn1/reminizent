46daaee34532fa953c14dfb3685f95ef447116a3
AmberSchatz <ambernschatz@gmail.com>
2018-05-02 14:22:35 -0700

    edit customer format fix w/ add of realtor

--- _frontend/scripts/components/admin-journey/edit-customer.js
+++ _frontend/scripts/components/admin-journey/edit-customer.js
@@ -74,22 +74,47 @@ class EditCustomer extends Component {
                     <div className="col-md-10 col-md-offset-1 editCustomerPage " >
                         <div style={{ margin: 15 }}>
                             <div className="row" >
-                                <h2 style={{ margin: 20 }}>Edit Customer</h2>
+                                <h2 style={{ margin: 20 }}>Edit Lead</h2>
                             </div>
+
                             <div className="form-group">
+
                                 <div className="row row-spacing">
-                                    <div className="form-inline col-md-11">
+
+                                    <div className="col-md-3">
+                                        <label htmlFor="" className="">First Name: </label>
                                         <input className="form-control input2" type="text" placeholder="First Name" value={this.state.customerObject.FirstName} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, FirstName: e.target.value } }) }} />
-                                        <input className="form-control input1" type="text" placeholder="Last Name" value={this.state.customerObject.LastName} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, LastName: e.target.value } }) }} />
-                                        <input className="form-control input1" type="email" name="" id="customerEmail" placeholder="Email Address" value={this.state.customerObject.Email} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, Email: e.target.value } }) }} />
-                                        <input className="form-control input1" type="text" placeholder="Phone Number" value={this.state.customerObject.Phone} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, Phone: e.target.value } }) }} />
                                     </div>
+
+                                    <div className="col-md-3" >
+                                        <label htmlFor="">Last Name: </label>
+                                        <input className="form-control " type="text" placeholder="Last Name" value={this.state.customerObject.LastName} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, LastName: e.target.value } }) }} />
+                                    </div>
+
+                                    <div className="col-md-3">
+                                        <label htmlFor="">E-mail Address: </label>
+                                        <input className="form-control" type="email" name="" id="customerEmail" placeholder="Email Address" value={this.state.customerObject.Email} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, Email: e.target.value } }) }} />
+                                    </div>
+
+                                    <div className="col-md-3">
+                                        <label htmlFor="">Phone Number: </label>
+                                        <input className="form-control" type="text" placeholder="Phone Number" value={this.state.customerObject.Phone} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, Phone: e.target.value } }) }} />
+                                    </div>
+
+
                                 </div>
+
+
                                 <div className="row row-spacing">
-                                    <div className="form-inline col-md-11">
-                                        <input className="form-control input2" type="text" placeholder="Company" value={this.state.customerObject.Company} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, Company: e.target.value } }) }} />
+
+                                    <div className="col-md-3">
+                                        <label htmlFor="">Company: </label>
+                                        <input className="form-control " type="text" placeholder="Company" value={this.state.customerObject.Company} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, Company: e.target.value } }) }} />
+                                    </div>
+
+                                    <div className="col-md-3">
                                         <div className="form-group">
-                                            <label htmlFor="email" className="input1">Interest:</label>
+                                            <label htmlFor="email" className="">Interest:</label>
                                             <select className="form-control" value={this.state.customerObject.AreaOfInterest} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, AreaOfInterest: e.target.value } }) }}>
                                                 <option>General Information</option>
                                                 <option>Loan Information</option>
@@ -97,8 +122,11 @@ class EditCustomer extends Component {
                                                 <option>Market Trends</option>
                                             </select>
                                         </div>
+                                    </div>
+
+                                    <div className="col-md-3">
                                         <div className="form-group">
-                                            <label htmlFor="" className="input1">Referral Type</label>
+                                            <label htmlFor="" className="">Referral Type:</label>
                                             <select className="form-control" value={this.state.customerObject.HeardAbout} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, HeardAbout: e.target.value } }) }}>
                                                 <option defaultValue>Select ...</option>
                                                 <option>TV</option>
@@ -107,51 +135,80 @@ class EditCustomer extends Component {
                                                 <option>Walk-in</option>
                                                 <option>Referral</option>
                                             </select>
-                                            <label htmlFor="" className="input1">Name of Referral</label>
-                                            <input className="form-control" type="text" placeholder="" value={this.state.customerObject.Referral} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, Referral: e.target.value } }) }} />
                                         </div>
                                     </div>
-                                </div>
-                                <div className="row row-spacing">
-                                    <div className="form-inline col-md-11">
-                                        <label htmlFor="" className="input1">Application Status</label>
-                                        <select className="form-control" value={this.state.customerObject.ApplicationStatus} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, ApplicationStatus: e.target.value } }) }}>
-                                            <option>Purchase only</option>
-                                            <option>Looking</option>
-                                            <option>Application for Pre-Qualified</option>
-                                            <option>Application for Pre-Approval</option>
-                                            <option>Looking - Pre-Qualified</option>
-                                            <option>Looking - Pre-Approved</option>
-                                            <option>In Contract</option>
-                                        </select>
-                                        <label htmlFor="" className="input1">Loan Status</label>
-                                        <select className="form-control" value={this.state.customerObject.LoanStatus} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, LoanStatus: e.target.value } }) }}>
-                                            <option>Application</option>
-                                            <option>Initial Documents</option>
-                                            <option>Setup / Initial Disclosures</option>
-                                            <option>Upfront Underwriting</option>
-                                            <option>Processing</option>
-                                            <option>Final Underwrite</option>
-                                            <option>Closing</option>
-                                            <option>Closed</option>
-                                            <option>Withdrawn</option>
-                                            <option>Denied</option>
-                                            <option>Suspended</option>
-                                        </select>
+
+                                    <div className="col-md-3">
+                                        <label htmlFor="" className="">Name of Referral:</label>
+                                        <input className="form-control" type="text" placeholder="" value={this.state.customerObject.Referral} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, Referral: e.target.value } }) }} />
                                     </div>
-                                </div>
-                                <div className="row" style={{ margin: 10, paddingBottom: 10 }} >
-                                    {/* <div className="row" style={{ paddingBottom: 10 }} > */}
-                                    <div className="col-md-8 col-md-offset-9">
-                                        <button className="btn col-md-2 " onClick={this.previousPg.bind(this)}>Cancel</button>
-                                        <button className="btn col-md-2 " style={{ marginLeft: 14 }} onClick={this.onSave.bind(this)}>Submit</button>
-                                    </div>
+
                                 </div>
                             </div>
+
+                            <div className="row row-spacing">
+
+                                <div className="col-md-3">
+                                    <label htmlFor="referralType">Real Estate Agent:</label>
+                                    <select className="form-control" >
+                                        <option defaultValue>Select ...</option>
+                                        <option>Max Power</option>
+                                        <option>Eddie Money</option>
+                                        <option>Mo House</option>
+                                        <option>I don't have one yet</option>
+                                    </select>
+                                </div>
+
+                                <div className="col-md-3">
+                                    <label htmlFor="" className="">Application Status:</label>
+                                    <select className="form-control" value={this.state.customerObject.ApplicationStatus} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, ApplicationStatus: e.target.value } }) }}>
+                                        <option>Purchase only</option>
+                                        <option>Looking</option>
+                                        <option>Application for Pre-Qualified</option>
+                                        <option>Application for Pre-Approval</option>
+                                        <option>Looking - Pre-Qualified</option>
+                                        <option>Looking - Pre-Approved</option>
+                                        <option>In Contract</option>
+                                    </select>
+                                </div>
+
+                                <div className="col-md-3">
+                                    <label htmlFor="" className="">Loan Status:</label>
+                                    <select className="form-control" value={this.state.customerObject.LoanStatus} onChange={(e) => { this.setState({ customerObject: { ...this.state.customerObject, LoanStatus: e.target.value } }) }}>
+                                        <option>Application</option>
+                                        <option>Initial Documents</option>
+                                        <option>Setup / Initial Disclosures</option>
+                                        <option>Upfront Underwriting</option>
+                                        <option>Processing</option>
+                                        <option>Final Underwrite</option>
+                                        <option>Closing</option>
+                                        <option>Closed</option>
+                                        <option>Withdrawn</option>
+                                        <option>Denied</option>
+                                        <option>Suspended</option>
+                                    </select>
+                                </div>
+
+                                <div className="col-md-3">
+                                </div>
+
+                            </div>
+
+
+                            <div className="row" style={{ margin: 10, paddingBottom: 10 }} >
+                                {/* <div className="row" style={{ paddingBottom: 10 }} > */}
+                                <div className="col-md-8 col-md-offset-9">
+                                    <button className="btn col-md-2 " onClick={this.previousPg.bind(this)}>Cancel</button>
+                                    <button className="btn col-md-2 " style={{ marginLeft: 14 }} onClick={this.onSave.bind(this)}>Submit</button>
+                                </div>
+                            </div>
+
+
                         </div>
                     </div>
                 </div>
             </div>
+            // </div >
         )
     }
 }
